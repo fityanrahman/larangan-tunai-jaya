4fc2c4560d916129ec00120730c34189cbf36b86
fityanrahman <fi.elrahman6@gmail.com>
2023-04-05 09:31:39 +0700

    added string consts

---
+++ lib/const/const_strings.dart
@@ -0,0 +1,4 @@
+const String thankInstallment = 'Thank you for choosing installment payment option. After payment complete, your vehicle will be delivered by tomorrow.';
+const String thankCash = 'Cash payment option may cause delay on your vehicle delivery by 9 months or later. Otherwise use installment for instant delivery.';
+const String orderSuccess = 'Your vehicle will be in front of your house by tomorrow';
+const String orderFailed = 'Please try again later. Otherwise select installment payment option.';
